70991580db9723848dca96452c25b2933b733412
Darth Varg <cbvargas123@gmail.com>
2020-03-17 22:15:28 -0700

    After altering the sort method for 'createdAt' sorts, all tests pass; Need queries for updating 'unhelpful' and 'helpful' counts

--- database/models.js
+++ database/models.js
@@ -3,8 +3,8 @@ const Review = require('./schemas.js');
 module.exports = {
     getAll: () => Review.find({}),
     getMostHelpful: () => Review.find({}).sort({"helpfulCount": -1}),
-    getRecent: () => Review.find({}).sort({"createdAt": 1}),
-    getOld: () => Review.find({}).sort({"createdAt": -1}),
+    getRecent: () => Review.find({}).sort({"createdAt": -1}),
+    getOld: () => Review.find({}).sort({"createdAt": 1}),
     getHighRate: () => Review.find({}).sort({"rating": -1}),
     getLowRate: () => Review.find({}).sort({"rating": 1})
 }
