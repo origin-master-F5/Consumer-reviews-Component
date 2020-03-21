a904653c60b3b7d0c7dbcf2abb3068f8f35dac51
Darth Varg <cbvargas123@gmail.com>
2020-03-21 14:54:46 -0700

    Snapshot, Gallery, Filter and Review are now all Stateful Components and have been added to the App Component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import Snapshot from './Snapshot.jsx';
+import Gallery from './Gallery.jsx';
+import Filter from './Filter.jsx';
+import Review from './Review.jsx';
+
 
 class App extends React.Component {
+    constructor(props) {
+        super(props)
+        
+        this.state = {}
+    }
     render() {
         return (
-            <div>
-                <h2>React is up and running!</h2>
+            <div className="reviews-accordion">
+                <h2>React is rendering</h2>
+                <Snapshot />
+                <Gallery />
+                <Filter />
+                <Review />
             </div>
         );
     }
---
+++ client/src/components/Gallery.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+class Gallery extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {}
+    }
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default Gallery
