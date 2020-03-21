40487988e761fe82aab3d599b647f2da72b4a7d8
Darth Varg <cbvargas123@gmail.com>
2020-03-20 20:26:13 -0700

    Finished configuring webpack; Created two starter React files; React is rendering on the DOM

---
+++ client/src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>React is up and running!</h2>
+            </div>
+        );
+    }
+}
+
+export default App
