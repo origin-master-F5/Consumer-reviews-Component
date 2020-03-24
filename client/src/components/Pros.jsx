d1affa6e3af0b5e65c9bda924adeedd20edcd358
Darth Varg <cbvargas123@gmail.com>
2020-03-24 16:45:25 -0700

    Cleaned up old comments from CSS; Changed the wording of class name in Pros/Cons components

--- client/src/components/Pros.jsx
+++ client/src/components/Pros.jsx
@@ -5,7 +5,7 @@ const Pros = (props) => (
     <div className="compare-list">
         <div className="pros-title">Pros mentioned</div>
         {props.filters.map((word, index) => (
-            <button className="filter-btn" key={index}>{word[0]} ({word[1]})</button>
+            <button className="filter-word-btn" key={index}>{word[0]} ({word[1]})</button>
         ))}
     </div>
 );
