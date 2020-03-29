629272c840ca9f19a8d22d39e8fe7590264b8f03
Darth Varg <cbvargas123@gmail.com>
2020-03-29 15:56:51 -0700

    Images carousel now scrolls!

--- client/src/components/Gallery.jsx
+++ client/src/components/Gallery.jsx
@@ -8,8 +8,11 @@ class Gallery extends React.Component {
         super(props)
 
         this.state = {
-            reviews: []
+            reviews: [],
+            margin: 0
         }
+        this.scrollRight = this.scrollRight.bind(this)
+        this.scrollLeft = this.scrollLeft.bind(this)
     }
     componentDidMount() {
         axios.get('/reviews')
@@ -19,23 +22,46 @@ class Gallery extends React.Component {
                 })
             })
     }
+    scrollRight() {
+        this.setState({
+            margin: this.state.margin - 648
+        })
+    }
+    scrollLeft() {
+        this.setState({
+            margin: this.state.margin + 648
+        })
+    }
     render() {
         return (
             <div className="gallery-wrapper  bottom-border-line">
                 <h3 className="gallery-title">Customer images</h3>
                 <div className="image-carousel">
                     <div className="carousel-wrapper">
-                        <img className="left-caret" src="/images/left-chevron-blue.png" />
+                        {
+                            this.state.margin
+                                ?
+                                <img className="left-caret" src="/images/left-chevron-blue.png" onClick={this.scrollLeft} />
+                                :
+                                <img className="left-caret-grey" src="/images/left-chevron-grey.png" />
+                        }
                         <ul className="image-list">
+                            <li className="img-list-item" style={{ marginLeft: `${this.state.margin}px` }}></li>
                             {this.state.reviews.map((review, index) => {
                                 if (review.pics.length > 0) {
-                                   return review.pics.map((pic) => (
+                                    return review.pics.map((pic) => (
                                         <Pic key={pic._id} id={review._id} url={pic.url} />
                                     ))
                                 }
                             })}
                         </ul>
-                        <img className="right-caret" src="/images/right-chevron-blue.png" />
+                        {
+                            this.state.margin <= -648
+                                ?
+                                <img className="right-caret-grey" src="/images/right-chevron-grey.png" />
+                                :
+                                <img className="right-caret" src="/images/right-chevron-blue.png" onClick={this.scrollRight} />
+                        }
                     </div>
                 </div>
             </div>
